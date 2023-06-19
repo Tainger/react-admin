b9ff46c9ef0c01e2e168333b10a2ac2af76b277d
rocky <1131819936@qq.com>
2023-06-19 22:56:54 +0800

    列表渲染

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-
-
 const name = 'rocky'
 
 const skill = () => {
-  return '放大招'
+    return '放大招'
 }
+const song = [
+    {
+        id: 1, name: '知心绝对'
+    },
+    {
+        id: 2, name: '像我这样人'
+    },
+    {
+        id: 3, name: '南山南'
+    }
 
+]
 const flag = true
+
 function App() {
-  return (
-    <div className="App">
-      {name}
-      {skill()}
-      {flag ? '很聪明':'很笨'}
-    </div>
-  );
+    return (
+        <div className="App">
+            <ul>
+                {song.map(song => <li key={song.id}>{song.name} </li>)}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
