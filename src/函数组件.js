1551d56d66bd07ec03b31713f141747ed086ab95
rocky <1131819936@qq.com>
2023-06-20 23:54:04 +0800

    函数组件

---
+++ src/函数组件.js
@@ -0,0 +1,14 @@
+function Hello (){
+
+    return (<div> hello</div>)
+}
+
+function App() {
+    return (
+        <div className="App">
+           <Hello></Hello>
+        </div>
+    );
+}
+
+export default App;
