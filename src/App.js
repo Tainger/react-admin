4a74b1a83a35c82936a75cd41b335b6e32afd8c1
rocky <1131819936@qq.com>
2023-06-21 00:29:45 +0800

    传递事件和消息

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
 class HelloComponent extends React.Component {
+
+     event2 = (e, msg) => {
+         e.preventDefault()
+         console.log(e)
+        console.log(msg)
+    }
+
     render() {
-        return (<div> Hello Component </div>)
+        return (<div onClick={(e)=>this.event2(e, 'hell0')}> <a href= "https://www.baidu.com"> 百度一下</a></div>)
     }
 }
 
