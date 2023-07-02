19012e560506ed02c23b16c37b8327bc505218f2
rocky <1131819936@qq.com>
2023-07-02 14:41:14 +0800

    feat: 增加校验

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -8,13 +8,16 @@ function Login() {
             <Card className="login-container">
                 <img className="login-logo" src={logo} alt=""/>
                 <Form className="login-form">
-                    <Form.Item>
+                    <Form.Item name="mobile" rules={[{required: true, message: '请输入手机号'}, {
+                        pattern: /^1[3-9]\d{9}$/,
+                        message: '输入的手机号格式不对'
+                    }]}>
                         <Input size="large" placeholder="请输入手机号"/>
                     </Form.Item>
 
 
                     <Form.Item>
-                        <Input size="large" placeholder="请输入密码"/>
+                        <Input size="large" placeholder="请输入密码" rules={[{required: true, message: '请输入手机号'}]}/>
                     </Form.Item>
                     <Form.Item name="remember" valuePropName="checked">
                         <Checkbox className="login-checkbox-label" defaultChecked="true">
